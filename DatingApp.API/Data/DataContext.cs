670c5b264f11eba456319a17fe509b9730ec7526
Carlos Robles <carlosrobles.be@gmail.com>
2020-01-09 11:47:24 -0300

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        //El nombre del DbSet "Values" será el nombre que se le dá a la tabla en la BD
+        public DbSet<Value> Values { get; set; }
+    }
+}
